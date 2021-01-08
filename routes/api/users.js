ab7cb1d6c06b8e83a76b9065b1753411cbcd3998
Vk-Demon <53090982+Vk-Demon@users.noreply.github.com>
2021-01-08 17:07:47 +0530

    commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,19 +14,33 @@ const User = require('../../models/User');
 router.post('/', (req, res) => {
     const { name, email, password } = req.body;
 
-    // Simple validation
+    // Input(non-empty) validation
     if(!name || !email || !password){
-        return res.status(400).json({msg: 'Please enter all fields'});
+        return res.status(400).json({msg: '* Please enter all fields'});
     }
+
+    // Email Validation
+    if (!validateEmail(email))
+        return res.status(400).json({ msg: '* Invalid Email ID!' });
 
     // Password validation
     if (password.length < 8)
-        return res.status(400).json({ msg: 'Password must be at least 8 characters!' });
+        return res.status(400).json({ msg: '* Password must be at least 8 characters!' });
+    if (!pass_digit(password))
+        return res.status(400).json({ msg: '* Password must include at least one digit!' });
+    if (!pass_lowercase(password))
+        return res.status(400).json({ msg: '* Password must include at least one lowercase!' });
+    if (!pass_uppercase(password))
+        return res.status(400).json({ msg: '* Password must include at least one uppercase!' });
+    if (!pass_special_char(password))
+        return res.status(400).json({ msg: '* Password must include at least one special character!' });
+    if (!validatePassword(password))
+        return res.status(400).json({ msg: '* Password must not contain white spaces!' });
 
     // Check for existing user
     User.findOne({email})
         .then(user => {
-            if(user) return res.status(400).status({msg: 'User already exists'});
+            if(user) return res.status(400).status({msg: '* User already exists'});
 
             const newUser = new User({
                 name,
@@ -65,5 +79,42 @@ router.post('/', (req, res) => {
         })
 
 });
+
+// Email Validation
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+// Password Validation
+function pass_lowercase(password) {
+    const re = /^(?=.*[a-z]).*$/;
+    return re.test(password);
+}
+
+function pass_uppercase(password) {
+    const re = /^(?=.*[A-Z]).*$/;
+    return re.test(password);
+}
+
+function pass_special_char(password) {
+    const re = /^(?=.*[@#$%^&+=]).*$/;
+    return re.test(password);
+}
+
+function pass_digit(password) {
+    const re = /^(?=.*[0-9]).*$/;
+    return re.test(password);
+}
+
+function pass_no_white_space(password) {
+    const re = /^(?=\\S+$).*$/;
+    return re.test(password);
+}
+
+function validatePassword(password) {
+    const re = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/;
+    return re.test(password);
+}
 
 module.exports = router;
